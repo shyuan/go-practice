40af5a9e437c68f2f7a7b5525c245c67fe28a9a5
Chris Yuan <honglong@gmail.com>
2015-02-09 13:31:42 +0800

    Add example to marshal struct to json string.

---
+++ json-marshal/json-marshal.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type s struct {
+	Int       int
+	String    string
+	ByteSlice []byte
+}
+
+func main() {
+	a := &s{42, "Hello World!", []byte{0, 1, 2, 3, 4}}
+
+	out, err := json.Marshal(a)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(out))
+}
